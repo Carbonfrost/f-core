8e6e53a9dd2f0e7384aee98a84a0518c080b60fa
Gary Mann <garythemann@users.noreply.github.com>
2019-12-31 22:11:13 -0800

    Engineering system update

--- eng/.mk/help.mk
+++ eng/.mk/help.mk
@@ -1,12 +1,12 @@
 ## Show this help screen
 help:
-	@awk '/^[^-][/a-zA-Z\-\_0-9]+:/ { \
+	@ awk '/^[^-][/a-zA-Z\-\_0-9]+:/ { \
 		helpMessage = match(lastLine, /(.+)/); \
 		if (helpMessage) { \
 			helpCommand = substr($$1, 0, index($$1, ":")-1); \
 			helpMessage = lastLine; \
 			gsub(/##/, "\n            ", helpMessage); \
-			printf "  \033[36m%s$(RESET) %s\n", helpCommand, helpMessage; \
+			printf "  \033[36m%s\033[0m %s\n", helpCommand, helpMessage; \
 			lastLine = ""; \
 		} \
 	} \
